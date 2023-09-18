7ea99d769bb8f62d341cde91b5e7e579e45c2851
Harshal Dhote <dhoteh020@gmail.com>
2023-09-18 20:24:07 +0530

    third commit day6

--- hackerRank_practice/variableSizedArray.cpp
+++ hackerRank_practice/variableSizedArray.cpp
@@ -6,60 +6,117 @@
 using namespace std;
 
 
-// int main() {
-//     /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
-//     int n,q,k;
-    
-//     cin>>n;
-//     cin>>q;
-//     int a[n];
-//     for(int i=0;i<=n;i++){
-//         cin>>a[i];
-//         for(int k=0;i<k;i++){
-//             cin>>a[k];
-//     }
-//     }
+int main()
+{
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
 
-//     return 0;
-// }
+    int n, q;
+    cin >> n >> q;
+    // vector must be added.
 
-
-// int main() {
-//     int n ;
-//     cin >> n;
-//     int a[n] ;
-//     for(int i =  0 ; i<n ;i++)
-//     {
-//         cin >> a[i];
-//     }
-//     cout<<"\nok\n";
-//     for(int i = n-1 ; i>=0 ; i--){
-//         cout<<a[i]<<" "<<endl;
-
-//     }
-//     return 0;
-// }
-
-int main(){
-    int n,q;
-    scanf("%d %d", &n, &q);
-    int a[n][10000];
-    for(int i=0;i<n;i++){
-        int k;
-        cin>>k;
-        for(int j=0;j<k;j++){
-            cin>>a[i][j];
+    vector<vector<int> > a(n);
+    for (int i = 0; i < n; i++)
+    {
+        int s;
+        cin >> s;
+        a[i].resize(s);
+        for (int j = 0; j < s; j++)
+        {
+            cin >> a[i][j];
         }
     }
-    for(int i=0;i<q;i++){
-        int x,y;
-        cin>>x>>y;
-        if(x>=0 && x<n && y<300000 && y>=0){
-            cout<<a[x][y]<<endl;    
-        }
-        else{
-            cout<<"Invalid Query"<<endl;
-        }
-    }    
+    for (int k = 0; k < q; k++)
+    {   int i,j;
+        cin >>i>>j;
+        
+        cout << a[i][j] << endl;
+    }
+
     return 0;
 }
+
+// #include <cmath>
+// #include <cstdio>
+// #include <vector>
+// #include <iostream>
+// #include <algorithm>
+// using namespace std;
+
+// int main()
+// {
+//     /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+
+//     int n, q;
+//     cin >> n >> q;
+//     // vector must be added.
+
+//     vector<vector<int> > a(n);
+//     for (int i = 0; i < n; i++)
+//     {
+//         int s;
+//         cin >> s;
+//         a[i].resize(s);
+//         for (int j = 0; j < s; j++)
+//         {
+//             cin >> a[i][j];
+//         }
+//     }
+//     for (int k = 0; k < q; k++)
+//     {   int i,j;
+//         cin >>i>>j;
+        
+//         cout << a[i][j] << endl;
+//     }
+
+//     return 0;
+// }
+
+// // int main() {
+// //     int n ;
+// //     cin >> n;
+// //     int a[n] ;
+// //     for(int i =  0 ; i<n ;i++)
+// //     {
+// //         cin >> a[i];
+// //     }
+// //     cout<<"\nok\n";
+// //     for(int i = n-1 ; i>=0 ; i--){
+// //         cout<<a[i]<<" "<<endl;
+
+// //     }
+// //     return 0;
+// // }
+
+// // // int main() {
+// // //     /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+// // //     int n,q,k;
+
+// // //     cin>>n;
+// // //     cin>>q;
+// // //     int a[n];
+// // //     for(int i=0;i<=n;i++){
+// // //         cin>>a[i];
+// // //         for(int k=0;i<k;i++){
+// // //             cin>>a[k];
+// // //     }
+// // //     }
+
+// // //     return 0;
+// // // }
+
+// // // int main() {
+// // //     int n ;
+// // //     cin >> n;
+// // //     int a[n] ;
+// // //     for(int i =  0 ; i<n ;i++)
+// // //     {
+// // //         cin >> a[i];
+// // //     }
+// // //     cout<<"\nok\n";
+// // //     for(int i = n-1 ; i>=0 ; i--){
+// // //         cout<<a[i]<<" "<<endl;
+
+// // //     }
+// // //     return 0;
+// // // }
+
